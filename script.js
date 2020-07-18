d1057c786d428415ccbbaa0d8ad1ffb15015c5bb
GiovaniCenta <geeowdk@gmail.com>
2020-07-18 11:14:14 -0300

    SP done

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
 let buttons= document.querySelectorAll("#container-buttons");
 let messageContainer = document.querySelector("#message")
-let messageText = document.querySelector("message p");
+let messageText = document.querySelector("#message p");
 let SecondPlayer;
+let xScore = document.querySelector("#scoreboard-1");
+let oScore = document.querySelector("#scoreboard-2");
+let p1w=0;
+let p2w=0;
 
-function playFunction(player1,player2){ //who's going to play and draw X or O in the boxes
-    let p1w=0;
-    let p2w=0;
-    let counterFail=0;   
-
-    //while(p1w<3 && p2w<3){
-        for(let i=0;i<boxes.length;i++)
-        {
-
+function playFunction(){ //who's going to play and draw X or O in the boxes
+    let player1=0;
+    let player2=0;
+    let counterFail=0;
+    let win=-1; 
+    let res=-1;
+    
+    let r=-1;
+    for(let i=0;i<boxes.length;i++){
         //when someone click inside the box
         boxes[i].addEventListener("click",function(){
             let play;
@@ -32,22 +36,33 @@ function playFunction(player1,player2){ //who's going to play and draw X or O in
                 win = roundVictoryCheck();
                 console.log("win: " + win);
                 if ( win == 1){
-                    p1w=p1w+1;
-                    console.log("player 1 wins: " + p1w);}
+                    //p1w=p1w+1;
+                    console.log("player 1 wins: ");
+                    scoreBoard(1);
+                    counterFail=0;
+                }
                 else if (win == 2 ){
-                    p2w=p2w+1;
-                    console.log("player 2 wins: " + p2w);}
+                    //p2w=p2w+1;
+                    console.log("player 2 wins: ");
+                    
+                    scoreBoard(2);
+                    counterFail=0;
+                    
+                }
                  counterFail++;
                  if (counterFail>8 && win ==0){
-                     console.log("Game ended without a winner");
-                 }
+                        scoreBoard(0);
+                        counterFail=0;
+                     }
 
                 }
-        });
+                if (r!=-1)
+                    return r;
+            });
+       
         }
-        //clear screen
-    //}
-    }
+    
+}
 
 function roundVictoryCheck(){
     
@@ -125,12 +140,12 @@ function check(...args){
 
 function roundWin(a,b,c) { //verifies if someone wons the round;
     if (a==b && b==c){
-        console.log("dsdasdas");
+        //console.log("dsdasdas");
         if (a =='x'){
             return 1;
         }
         else if (a =='o'){
-           console.log("player2");
+           //console.log("player2");
             return 2;
         }
     }
@@ -139,8 +154,63 @@ function roundWin(a,b,c) { //verifies if someone wons the round;
 
 }
 
+function scoreBoard(r){
+    if (r==1) //means player1 won
+    {
+        p1w++;
+        console.log("p1w: " + p1w);
+        xScore.textContent = p1w;
+        msg = "Player 1 wins!";
+        messageContainer.classList.remove("hide"); //remove hide to show the msg
+        messageText.innerHTML=msg;
+        setTimeout(function (){ //add class hide to hide the msg again after 3 seconds
+            messageContainer.classList.add("hide");
+            removeBoxes();
+        },3000);
+        
+        //clear screen
+    }
+    else if(r==2)
+    {
+        p2w++;
+        console.log("p2w: " + p2w);
+        oScore.textContent = p2w;
+        msg = "Player 2 wins!";
+        messageContainer.classList.remove("hide");//remove hide to show the msg
+        messageText.innerHTML=msg;
+        setTimeout(function (){ //add class hide to hide the msg again after 3 seconds
+            messageContainer.classList.add("hide");
+            removeBoxes();
+        },3000);
+        
+        //clear screen
+
+    }
+    else if(r==0){
+        msg = "Round ended w/o winner!";
+        messageContainer.classList.remove("hide");//remove hide to show the msg
+        messageText.innerHTML=msg;
+        setTimeout(function (){ //add class hide to hide the msg again after 3 seconds
+            messageContainer.classList.add("hide");
+            removeBoxes();
+        },3000);
+
+    }
+
+
+
+}
+
+
+
+function removeBoxes(){ //function to remove x's and o's from the boxes
+    let boxesToRemove=document.querySelectorAll(".box div");
+    for(let i=0;i<boxesToRemove.length;i++){
+        boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+    }
+}
 
 
 let player1=0;
 let player2=0;
-playFunction(player1,player2);
+playFunction();
