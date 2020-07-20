2604e397ae070a0bf30dabb2875511c4f5e22c7e
GiovaniCenta <geeowdk@gmail.com>
2020-07-20 11:28:15 -0300

    Done! Last addition : single player mode

--- script.js
+++ script.js
@@ -1,69 +1,91 @@
+//=========================HTML selectors=========================
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let buttons= document.querySelectorAll("#container-buttons");
+let buttons= document.querySelectorAll("#container-buttons button");
 let messageContainer = document.querySelector("#message")
 let messageText = document.querySelector("#message p");
 let SecondPlayer;
 let xScore = document.querySelector("#scoreboard-1");
 let oScore = document.querySelector("#scoreboard-2");
-let p1w=0;
+//===================================================================
+let p1w=0; //number of wins to update the scoreboard
 let p2w=0;
 
-function playFunction(){ //who's going to play and draw X or O in the boxes
-    let player1=0;
-    let player2=0;
-    let counterFail=0;
+
+function playFunction(mode){ //who's going to play and draw X or O in the boxes
+                    //mode: against the cpu or against another player? 
+    let player1=0;   //number of plays of player 1
+    let player2=0;  ///number of plays of player 2
+    let counterFail=0; //counter to check if the round resulted in a tie
     let win=-1; 
-    let res=-1;
     
-    let r=-1;
     for(let i=0;i<boxes.length;i++){
         //when someone click inside the box
         boxes[i].addEventListener("click",function(){
-            let play;
+            let play =checkPlay(player1,player2); //check if draw X or O
             if (this.childNodes.length==0){ //if this box isnt occupied
-                if (player1==player2){ //player1 starts playing
-                    play=x;
-                    player1++;
-                }else{
-                    play=o;
-                    player2++;
-                }
                 let clonePlay = play.cloneNode(true); //save "play" to posterious use;
                 this.appendChild(clonePlay);//drawning play to the box
+                
+                if (player1==player2){ //player1 starts playing
+                    
+                    player1++;
+                    if(mode=="sp"){  //if the mode selected is single player the program will execute the recursive function cpuPlay to generate random numbers and draw O's
+                        
+                        cpuPlay();
+                        player2++;
+                        counterFail++;
+                        console.log(counterFail);
+                    }
+
+
+                }else{
+                    
+                    player2++;
+                }
+                
                 let win=0;
                 win = roundVictoryCheck();
                 console.log("win: " + win);
                 if ( win == 1){
-                    //p1w=p1w+1;
                     console.log("player 1 wins: ");
                     scoreBoard(1);
                     counterFail=0;
                 }
                 else if (win == 2 ){
-                    //p2w=p2w+1;
                     console.log("player 2 wins: ");
-                    
                     scoreBoard(2);
                     counterFail=0;
                     
                 }
                  counterFail++;
-                 if (counterFail>8 && win ==0){
+                 if (counterFail>8 && win ==0){ //counterfail counts the numbers of plays to determine if there are plays left, if doesnt, will return 0 to scoreboard,equals tie 
                         scoreBoard(0);
                         counterFail=0;
                      }
 
                 }
-                if (r!=-1)
-                    return r;
+                
             });
        
         }
     
 }
 
+//whos going to be draw,X or O
+function checkPlay(player1,player2){
+    if(player1 == player2) {
+        play = x;
+      } else {
+        play = o;
+      }
+    
+      return play;
+
+}
+
+//check line by line
 function roundVictoryCheck(){
     
     
@@ -119,6 +141,7 @@ function roundVictoryCheck(){
         
 }
 
+//generic function to determine if there are boxes filled to check a possible win
 function check(...args){
     if(args[0].childNodes.length>0 && args[1].childNodes.length>0 && args[2].childNodes.length>0){ //verify if the blocks contains any play
         let getA = args[0].childNodes[0].className; //getA,B,C receives the classname of the play
@@ -138,15 +161,15 @@ function check(...args){
     return 0;
 }
 
-function roundWin(a,b,c) { //verifies if someone wons the round;
+//verifies if someone won the round;
+function roundWin(a,b,c) { 
     if (a==b && b==c){
-        //console.log("dsdasdas");
+        
         if (a =='x'){
             return 1;
         }
         else if (a =='o'){
-           //console.log("player2");
-            return 2;
+          return 2;
         }
     }
     else
@@ -154,6 +177,7 @@ function roundWin(a,b,c) { //verifies if someone wons the round;
 
 }
 
+//Update scoreboard after a round win
 function scoreBoard(r){
     if (r==1) //means player1 won
     {
@@ -168,7 +192,7 @@ function scoreBoard(r){
             removeBoxes();
         },3000);
         
-        //clear screen
+        
     }
     else if(r==2)
     {
@@ -183,7 +207,7 @@ function scoreBoard(r){
             removeBoxes();
         },3000);
         
-        //clear screen
+        
 
     }
     else if(r==0){
@@ -210,7 +234,57 @@ function removeBoxes(){ //function to remove x's and o's from the boxes
     }
 }
 
+//2 players or against the cpu?
+function spOr2p(){
+    for(let i=0; i<buttons.length;i++){
+        buttons[i].addEventListener("click",function(){
+            SecondPlayer = this.getAttribute("id");
+            console.log(SecondPlayer);
+            for(let j=0;j<buttons.length;j++){
+                buttons[j].style.display='none'; //hide buttons
+            };
+            setTimeout(function(){
+                let container = document.querySelector("#container");
+                container.classList.remove("hide");
+                playFunction(SecondPlayer);
+            })
+        })
 
-let player1=0;
-let player2=0;
-playFunction();
+    }
+}
+
+
+//function to make cpu play
+function cpuPlay(){
+    let cloneO = o.cloneNode(true);
+    let counter=0;
+  
+    
+    let filled=0;
+    //only fill boxes without child
+    for(let i=0;i<boxes.length;i++){
+        let randomNumber = Math.floor(Math.random()*5); //generate one random number, the box to be filled number;
+        if (boxes[i].childNodes[0]== undefined){
+            if(randomNumber<=1){
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;}
+            //check how many are filled;
+            }else{
+                filled++;
+            }
+
+        }
+
+    
+    
+    if(counter==0 && filled<9){
+       
+        cpuPlay();
+    }
+}
+
+
+// =========================================================================== PROGRAM MAIN ======================================================================================================
+
+spOr2p(); //first thing to do is to check game mode
